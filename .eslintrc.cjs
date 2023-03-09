cde54390a792934f910c3bc210039af87c5e03a9
Nwakakukaks <winnernwakaku123@gmail.com>
2023-03-09 12:19:17 +0000

    final update

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:snarkyjs/recommended'],
+  ignorePatterns: ['*.md'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['@typescript-eslint', 'snarkyjs'],
+  rules: {
+    'no-constant-condition': 'off',
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Member } from './member.js';
+import { Guardian, GuardianZkApp } from './guardian.js';
+import { Wallet } from './wallet.js';
+
+export { Guardian, GuardianZkApp, Member, Wallet };
